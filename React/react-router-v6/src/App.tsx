beb2fef245970f604a3bda1bdd02d1ff0ad6a65e
lioGR <liorgrishinnew@gmail.com>
2022-08-17 20:31:42 +0300

    React Router on nav bar

--- React/react-router-v6/src/App.tsx
+++ React/react-router-v6/src/App.tsx
@@ -3,16 +3,26 @@ import {
   Routes,
   Route,
 } from "react-router-dom"
-import './App.css';
+import './App.scss';
 
-import Navbar from "./views/components/Navbar";
+import Main from "./views/pages/main/Main";
+
+import Home from "./views/pages/home/Home";
+import About from "./views/pages/about/About";
+import Faq from "./views/pages/faq/Faq";
+import Contact from "./views/pages/contact/Contact";
+
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Navbar/>
-          {/* <Route path="/" element={<Navbar />}/> */}
+          <Route path="/" element={<Main />}>
+            <Route index element={<Home/>}/>
+            <Route path="about" element={<About/>}/>
+            <Route path="faq" element={<Faq/>}/>
+            <Route path="contact" element={<Contact/>}/>
+          </Route>
         </Routes>
       </BrowserRouter>
   );
--- React/react-router-v6/src/views/components/Navbar.tsx
+++ React/react-router-v6/src/views/components/Navbar.tsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="Navbar">
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>FAQ</li>
-                <li>Contact</li>
+            <ul className='Navbar-items'>
+                <Link to="/"><li className='Navbar-item'>Home</li></Link>
+                <Link to="/about"><li className='Navbar-item'>About</li></Link>
+                <Link to="/faq"><li className='Navbar-item'>FAQ</li></Link>
+                <Link to="/contact"><li className='Navbar-item'>Contact</li></Link>
             </ul>
         </nav>
     );
