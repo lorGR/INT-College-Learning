b08b2e073ed16b51d933b05258c639f9156e9d48
lioGR <liorgrishinnew@gmail.com>
2022-08-18 14:14:01 +0300

    Font Awesome Styles

--- React/Home Exersices/17-8-22/src/views/components/Navbar.tsx
+++ React/Home Exersices/17-8-22/src/views/components/Navbar.tsx
@@ -2,15 +2,28 @@ import { Link } from "react-router-dom";
 
 //Font Awesome imports
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
+
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <ul className="navbar__items">
-                <Link to="/"><li className="navbar__item"><FontAwesomeIcon icon={faHouse} /></li> </Link>
-                <Link to="/cart"><li className="navbar__item">Cart</li></Link>
-                <Link to="/profile"><li className="navbar__item">Profile</li></Link>
+                <Link to="/">
+                    <li className="navbar__item">
+                        <FontAwesomeIcon icon={faHouse} size="3x"/>
+                    </li>
+                </Link>
+                <Link to="/cart">
+                    <li className="navbar__item">
+                        <FontAwesomeIcon icon={faCartShopping} size="3x" />
+                    </li>
+                </Link>
+                <Link to="/profile">
+                    <li className="navbar__item">
+                        <FontAwesomeIcon icon={faUser} size="3x"/>
+                    </li>
+                </Link>
             </ul>
         </nav>
     );
