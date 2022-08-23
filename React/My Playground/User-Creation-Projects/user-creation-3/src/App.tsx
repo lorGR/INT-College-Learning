e72296c5c9dd09ee700f6da36136594c351d6518
lioGR <liorgrishinnew@gmail.com>
2022-08-23 15:49:32 +0300

    User Creation Ultimate Version
    All Types are spesific for thier element

--- React/My Playground/User-Creation-Projects/user-creation-3/src/App.tsx
+++ React/My Playground/User-Creation-Projects/user-creation-3/src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import Register from './features/Register';
+
 import './App.css';
 
+export interface User {
+  username: string,
+  password: string
+}
+
 function App() {
+
+  const handleGetUser = (user: User) => {
+    console.log(user);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Register onSubmit={handleGetUser} />
     </div>
   );
 }
