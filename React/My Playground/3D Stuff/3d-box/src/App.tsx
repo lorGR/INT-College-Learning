8493e17a23f76541a317d3a782e001f6c63a17df
lioGR <liorgrishinnew@gmail.com>
2022-08-26 17:23:20 +0300

    started 3d cube

---
+++ React/My Playground/3D Stuff/3d-box/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="cube">
+          <div className="face front"></div>
+          <div className="face right"></div>
+          <div className="face back"></div>
+          <div className="face left"></div>
+          <div className="face top"></div>
+          <div className="face bottom"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
