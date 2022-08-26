3f1cfb6b5b724a43345ed0ea242e4b2a9018a5a1
lioGR <liorgrishinnew@gmail.com>
2022-08-26 17:47:04 +0300

    3d cube complated

--- React/My Playground/3D Stuff/3d-box/src/App.tsx
+++ React/My Playground/3D Stuff/3d-box/src/App.tsx
@@ -5,12 +5,12 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="cube">
-          <div className="face front"></div>
-          <div className="face right"></div>
-          <div className="face back"></div>
-          <div className="face left"></div>
-          <div className="face top"></div>
-          <div className="face bottom"></div>
+          <div className="face front">1</div>
+          <div className="face right">2</div>
+          <div className="face back">3</div>
+          <div className="face left">4</div>
+          <div className="face top">5</div>
+          <div className="face bottom">6</div>
         </div>
       </div>
     </div>
