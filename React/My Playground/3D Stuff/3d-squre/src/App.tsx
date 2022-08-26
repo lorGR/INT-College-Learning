cad8745030c2c5f181c941afb7d9331af75d94ca
lioGR <liorgrishinnew@gmail.com>
2022-08-26 18:52:58 +0300

    3D Square Created

---
+++ React/My Playground/3D Stuff/3d-squre/src/App.tsx
@@ -0,0 +1,33 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="square">
+          <div className="bFace front">
+            <img src="https://pps.whatsapp.net/v/t61.24694-24/207420334_777821919882835_7330941523359246050_n.jpg?ccb=11-4&oh=01_AVzcPCcs0C8rZGKFn47VIZBCccDzOV0Vb4PxxY_84NY7jQ&oe=6316AB2E" alt="alex" />
+          </div>
+          <div className="sFace right">
+          <img src="https://pps.whatsapp.net/v/t61.24694-24/207420334_777821919882835_7330941523359246050_n.jpg?ccb=11-4&oh=01_AVzcPCcs0C8rZGKFn47VIZBCccDzOV0Vb4PxxY_84NY7jQ&oe=6316AB2E" alt="alex" />
+          </div>
+          <div className="bFace back">
+          <img src="https://pps.whatsapp.net/v/t61.24694-24/207420334_777821919882835_7330941523359246050_n.jpg?ccb=11-4&oh=01_AVzcPCcs0C8rZGKFn47VIZBCccDzOV0Vb4PxxY_84NY7jQ&oe=6316AB2E" alt="alex" />
+          </div>
+          <div className="sFace left">
+          <img src="https://pps.whatsapp.net/v/t61.24694-24/207420334_777821919882835_7330941523359246050_n.jpg?ccb=11-4&oh=01_AVzcPCcs0C8rZGKFn47VIZBCccDzOV0Vb4PxxY_84NY7jQ&oe=6316AB2E" alt="alex" />
+          </div>
+          <div className="bFace top">
+          <img src="https://pps.whatsapp.net/v/t61.24694-24/207420334_777821919882835_7330941523359246050_n.jpg?ccb=11-4&oh=01_AVzcPCcs0C8rZGKFn47VIZBCccDzOV0Vb4PxxY_84NY7jQ&oe=6316AB2E" alt="alex" />
+          </div>
+          <div className="bFace bottom">
+          <img src="https://pps.whatsapp.net/v/t61.24694-24/207420334_777821919882835_7330941523359246050_n.jpg?ccb=11-4&oh=01_AVzcPCcs0C8rZGKFn47VIZBCccDzOV0Vb4PxxY_84NY7jQ&oe=6316AB2E" alt="alex" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
