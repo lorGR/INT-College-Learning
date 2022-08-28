adbf8baedcc5ed0484523783e1408b744c642c73
lioGR <liorgrishinnew@gmail.com>
2022-08-28 10:52:38 +0300

    Completed Tal's Challange

--- React/My Playground/3D Stuff/3d-box/src/App.tsx
+++ React/My Playground/3D Stuff/3d-box/src/App.tsx
@@ -1,10 +1,19 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [cubeRotation, setCubeRotation] = useState("");
+  const handleChnage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCubeRotation(`rotateX(${e.target.value}deg) rotateY(${e.target.value}deg)`);
+  }
+
   return (
     <div className="App">
+      <label htmlFor="cubeRotation">Rotation:</label>
+      <input onChange={handleChnage} type="range" name="cubeRotation" id="cubeRotation" min="0" max="360" />
       <div className="container">
-        <div className="cube">
+        <div className="cube" style={{transform: cubeRotation}}>
           <div className="face front">1</div>
           <div className="face right">2</div>
           <div className="face back">3</div>
