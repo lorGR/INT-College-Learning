2e1b61b1e9880d411b20d3b836c5a095009f5daa
lioGR <liorgrishinnew@gmail.com>
2022-09-09 17:47:34 +0300

    WIP

--- React/Home Exersices/07-09-22/server.ts
+++ React/Home Exersices/07-09-22/server.ts
@@ -4,18 +4,21 @@ import mongoose from "mongoose";
 
 const app: express.Application = express();
 
+app.use(express.json());
+app.use(express.static("client/build"));
+
 dotenv.config();
-const mongoURI= process.env.MONGO_URI;
+const mongoURI = process.env.MONGO_URI;
 
 const port: number = 3000;
-
-mongoose.connect(mongoURI)
-    .then(() => {
-        console.log("Connected to DB");
-    })
-    .catch(() => {
-        console.log("Failed connect to DB");
-    })
+if(mongoURI) {
+    mongoose
+        .connect(mongoURI)
+        .then(() => console.log("Connected to DB"))
+        .catch(() => console.log("Failed connect to DB"))
+} else {
+    console.log("Couldn't find MONGO URI");
+}
 
 app.listen(port, () => {
     console.log(`Server is running http://localhost:${port}/`);
