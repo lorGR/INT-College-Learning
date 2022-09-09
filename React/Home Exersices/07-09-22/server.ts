3ceeda8f80f1d3ebf7b471aac8834374bd47573d
lioGR <liorgrishinnew@gmail.com>
2022-09-09 17:41:03 +0300

    WIP

--- React/Home Exersices/07-09-22/server.ts
+++ React/Home Exersices/07-09-22/server.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app: express.Application = express();
 
 dotenv.config();
-const port: string = process.env.PORT;
+const mongoURI= process.env.MONGO_URI;
+
+const port: number = 3000;
+
+mongoose.connect(mongoURI)
+    .then(() => {
+        console.log("Connected to DB");
+    })
+    .catch(() => {
+        console.log("Failed connect to DB");
+    })
 
 app.listen(port, () => {
     console.log(`Server is running http://localhost:${port}/`);
