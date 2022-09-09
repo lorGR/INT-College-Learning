4fa4f388afb7b1569c7341c526e1df6283799747
lioGR <liorgrishinnew@gmail.com>
2022-09-09 12:54:55 +0300

    WIP

--- React/Home Exersices/07-09-22/server.ts
+++ React/Home Exersices/07-09-22/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app: express.Application = express();
+
+dotenv.config();
 const port: string = process.env.PORT;
 
 app.listen(port, () => {
-    
+    console.log(`Server is running http://localhost:${port}/`);
 });
