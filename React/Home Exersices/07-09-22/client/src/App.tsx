aa718e7b11f840180a440aba45783002bbd3eaf4
lioGR <liorgrishinnew@gmail.com>
2022-09-10 12:59:09 +0300

    WIP

--- React/Home Exersices/07-09-22/client/src/App.tsx
+++ React/Home Exersices/07-09-22/client/src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Login from "./features/users/login";
 import Register from "./features/users/register";
-import { MainPage } from './features/users/register/store/mainPage/MainPage';
-import { AddSet } from './features/users/register/store/addSet/AddSet';
+import { MainPage } from './features/store/mainPage/MainPage';
+import { AddSet } from './features/store/addSet/AddSet';
 
 function App() {
   return (
