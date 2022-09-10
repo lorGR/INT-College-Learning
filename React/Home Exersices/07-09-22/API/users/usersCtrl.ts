9373b0751a1cdda2c853490da0e84b45478331f3
lioGR <liorgrishinnew@gmail.com>
2022-09-10 12:02:05 +0300

    Update usersCtrl.ts

--- React/Home Exersices/07-09-22/API/users/usersCtrl.ts
+++ React/Home Exersices/07-09-22/API/users/usersCtrl.ts
@@ -4,6 +4,7 @@ export async function addUser(req: express.Request, res: express.Response){
     try {
         console.log("Hey from AddUser Controller");
         const { username, password , email } = req.body;
+        
         res.send({ username: username, password: password, email: email});
     } catch (error) {
         console.error(error);
