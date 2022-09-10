e31b4029912cbf336d9c8d8f6e03d8697c0c7c2c
Gili <gilimena95@gmail.com>
2022-09-10 15:21:31 +0300

    store main page
    show lego cards

--- React/Home Exersices/07-09-22/API/legos/legosRoutes.ts
+++ React/Home Exersices/07-09-22/API/legos/legosRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { addLegoSet } from "./legosCtrl";
+import { addLegoSet, getAllLegoSets } from "./legosCtrl";
 
 const router = express.Router();
 
-router.post('/addLegoSet', addLegoSet)
+router
+.post('/addLegoSet', addLegoSet)
+.get("/getAllLegoSets", getAllLegoSets)
 
 
 export default router;
--- React/Home Exersices/07-09-22/client/src/features/store/mainPage/MainPage.tsx
+++ React/Home Exersices/07-09-22/client/src/features/store/mainPage/MainPage.tsx
@@ -1,11 +1,33 @@
 
 import { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import { LegoSetCard } from '../legoSetCard/LegoSetCard';
+
+export interface LegoSet {
+    setName:string,
+    price:string,
+    imgSrc:string
+}
+
 export const MainPage = () => {
-    // useEffect()
+    const[legoSetArray, setLegoSetArray] = useState<LegoSet[]>([])
+    useEffect(() => {
+        console.log("use Effect")
+        const getAllLegoSets = async () => {
+            const {data} = await axios.get("/legos/getAllLegoSets");
+            console.log(data)
+            const {legoSetArrayDB} = data;
+            console.log(legoSetArrayDB)
+            setLegoSetArray(legoSetArrayDB)
+        }
+        getAllLegoSets()
+    }, [])
     return (
         <div>
             <div>
                 <h1>Lego store</h1>
+                {legoSetArray.map((legoset) => <LegoSetCard setName={legoset.setName} price={legoset.price} imgSrc={legoset.imgSrc}/>)}
             </div>
         </div>
     );
