403c8520d120e80203566ea94466e8ffe561808d
lioGR <liorgrishinnew@gmail.com>
2022-09-11 13:59:18 +0300

    wip

--- React/My Playground/3D Stuff/3d-cube-spining/src/App.tsx
+++ React/My Playground/3D Stuff/3d-cube-spining/src/App.tsx
@@ -1,19 +1,31 @@
 import './App.css';
 
 function App() {
-  
-  
+
+
 
   return (
     <div className="App">
       <div className="container">
         <div className="cube" id='cube'>
-          <div className="faces front"></div>
-          <div className="faces right"></div>
-          <div className="faces back"></div>
-          <div className="faces left"></div>
-          <div className="faces top"></div>
-          <div className="faces bottom"></div>
+          <div className="faces front">
+            <img src="https://thumbs.dreamstime.com/b/wooden-crate-front-view-cargo-box-texture-d-rendering-205221256.jpg" alt="box" />
+          </div>
+          <div className="faces right">
+            <img src="https://thumbs.dreamstime.com/b/wooden-crate-front-view-cargo-box-texture-d-rendering-205221256.jpg" alt="box" />
+          </div>
+          <div className="faces back">
+            <img src="https://thumbs.dreamstime.com/b/wooden-crate-front-view-cargo-box-texture-d-rendering-205221256.jpg" alt="box" />
+          </div>
+          <div className="faces left">
+            <img src="https://thumbs.dreamstime.com/b/wooden-crate-front-view-cargo-box-texture-d-rendering-205221256.jpg" alt="box" />
+          </div>
+          <div className="faces top">
+            <img src="https://thumbs.dreamstime.com/b/wooden-crate-front-view-cargo-box-texture-d-rendering-205221256.jpg" alt="box" />
+          </div>
+          <div className="faces bottom">
+            <img src="https://thumbs.dreamstime.com/b/wooden-crate-front-view-cargo-box-texture-d-rendering-205221256.jpg" alt="box" />
+          </div>
         </div>
       </div>
     </div>
