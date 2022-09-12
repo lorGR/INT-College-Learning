84b9404394ae15718e13fed10e532f2a74819a7b
lioGR <liorgrishinnew@gmail.com>
2022-09-12 16:29:26 +0300

    WIP

--- React/Home Exersices/07-09-22/client/src/features/store/mainPage/MainPage.tsx
+++ React/Home Exersices/07-09-22/client/src/features/store/mainPage/MainPage.tsx
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import UserModel from "../../users/userModel";
 import { AddSet } from '../addSet/AddSet';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPlus} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 import { LegoSetCard } from '../legoSetCard/LegoSetCard';
 
@@ -38,7 +38,7 @@ export const MainPage = () => {
         } catch (error) {
             console.error(error);
         }
-    },[]);
+    }, []);
 
     const handleAddItemForm = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         try {
@@ -53,20 +53,20 @@ export const MainPage = () => {
     }
     return (
         <div className='store_session'>
-                <div className='store_session__Nav'>
-                    <div className="store_session__Nav__logoWrapper">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/500px-LEGO_logo.svg.png" alt="lego logo"/>
-                        <h1>Lego store</h1>
-                    </div>
-                    {user && <h2>Hello {user.username} </h2>}
-                    {user?.role !== "member" && <button className='addSetButton' onClick={handleAddItemForm}>
-                    <FontAwesomeIcon icon={faPlus} size="1x"/>
-                        </button>}
-                    {showAddItemForm && <AddSet setShowAddItemForm={setShowAddItemForm} setLegoSetArray={setLegoSetArray}/> }  
+            <div className='store_session__Nav'>
+                <div className="store_session__Nav__logoWrapper">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/500px-LEGO_logo.svg.png" alt="lego logo" />
+                    <h1>Lego store</h1>
                 </div>
-                <div className="store_session__legoSetContainer">
-                     {legoSetArray.map((legoset) => <LegoSetCard key={legoset._id} setLegoSetArray={setLegoSetArray} userRole={user?.role} setName={legoset.setName} price={legoset.price} imgSrc={legoset.imgSrc} />)}
-                </div>
+                {user && <h2>Hello {user.username} </h2>}
+                {user?.role !== "member" && <button className='addSetButton' onClick={handleAddItemForm}>
+                    <FontAwesomeIcon icon={faPlus} size="1x" />
+                </button>}
+                {showAddItemForm && <AddSet setShowAddItemForm={setShowAddItemForm} setLegoSetArray={setLegoSetArray} />}
+            </div>
+            <div className="store_session__legoSetContainer">
+                {legoSetArray.map((legoset) => <LegoSetCard key={legoset._id} setLegoSetArray={setLegoSetArray} userRole={user?.role} setName={legoset.setName} price={legoset.price} imgSrc={legoset.imgSrc} />)}
+            </div>
         </div>
     );
 };
