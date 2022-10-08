878b2a623759de9f7713a6964ac4721f08b43d98
lioGR <liorgrishinnew@gmail.com>
2022-10-08 10:44:44 +0300

    Update Register.tsx

--- Playground/Login-Register System/Login-Register.v1/client/src/features/register/Register.tsx
+++ Playground/Login-Register System/Login-Register.v1/client/src/features/register/Register.tsx
@@ -1,20 +1,32 @@
+import axios from "axios";
+import { useState } from "react";
 import { handleMatchPassword } from "./helper";
 
 const Register = () => {
 
+    const [registerStep1, setRegisterStep1] = useState(false);
+
     const handleChange = () => {
-            handleMatchPassword();
+        handleMatchPassword();
     }
 
-    const handleRegister = (event: React.FormEvent<HTMLFormElement> | any ) => {
+    const handleRegister = async (event: React.FormEvent<HTMLFormElement> | any) => {
         try {
             event.preventDefault();
-            const [ email, password, confirmPassword]: Array<string> = [
+            const [email, password, confirmPassword]: Array<string> = [
                 event.target.elements.email.value,
                 event.target.elements.password.value,
                 event.target.elements.confirmPassword.value
             ];
 
+            const { data } = await axios.post("/users/register-step-1", { email, password, confirmPassword });
+            if (!data) throw new Error("Couldn't receive data from axios POST : /users/register-step-1");
+            
+            const { error, register } = data;
+            if(error) throw error;
+
+            register && setRegisterStep1(true);
+            
             
         } catch (error) {
             console.error(error);
@@ -27,7 +39,7 @@ const Register = () => {
             <input type="email" name="email" id="email" placeholder="Enter email" />
             <input type="password" name="password" id="password" />
             <input onChange={handleMatchPassword} type="password" name="confirmPassword" id="confirmPassword" />
-            <input type="submit" value="Register" id="register"/>
+            <input type="submit" value="Register" id="register" />
         </form>
     )
 }
