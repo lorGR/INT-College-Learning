2f2c6a14ae5a8da31299c7c4f3af505e696ee89c
lioGR <liorgrishinnew@gmail.com>
2022-10-08 10:44:49 +0300

    Update UserCtrl.ts

--- Playground/Login-Register System/Login-Register.v1/server/features/users/UserCtrl.ts
+++ Playground/Login-Register System/Login-Register.v1/server/features/users/UserCtrl.ts
@@ -1,15 +1,21 @@
 import express from "express";
 import { UserValidation } from "./UserModel";
+import { emailExist } from "./UsersHelper";
 
 export async function registerStep1(req: express.Request, res: express.Response) {
     try {
         const { email, password, confirmPassword } = req.body;
         if (!email || !password || !confirmPassword) throw new Error("Missing fields: Email or Password or ConfirmPassword");
 
+        const emailIsExist = await emailExist(email);
+        if (emailIsExist) {
+            res.send({ register: false, error: "email already in use" });
+        }
+
         const { error } = UserValidation.validate({ email, password, confirmPassword });
         if (error) throw error;
 
-        res.send({ continue: true });
+        res.send({ register: true });
     } catch (error) {
         res.send({ error: error.message });
     }
