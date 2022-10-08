d873561d83464c725f0f171f12c634a5b97765ff
lioGR <liorgrishinnew@gmail.com>
2022-10-08 10:19:57 +0300

    WIP

---
+++ Playground/Login-Register System/Login-Register.v1/client/src/features/register/helper.ts
@@ -0,0 +1,18 @@
+export function handleMatchPassword () : void {
+    try {
+        const password = document.getElementById("password") as HTMLInputElement;
+        const confirmPassword = document.getElementById("confirmPassword") as HTMLInputElement;
+        const registerButton = document.getElementById("register") as HTMLInputElement;
+
+        if(confirmPassword.value !== password.value) {
+            password.style.borderColor = "red";
+            confirmPassword.style.borderColor = "red";
+            registerButton.disabled = true;
+        } else {
+            confirmPassword.style.borderColor = "green";
+            registerButton.disabled = false;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
