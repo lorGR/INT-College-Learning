7ef64d291e367365806ad180b47675091eb05c54
lioGR <liorgrishinnew@gmail.com>
2022-10-09 10:16:05 +0300

    Update Register.tsx

--- Playground/Login-Register System/Login-Register.v1/client/src/features/register/Register.tsx
+++ Playground/Login-Register System/Login-Register.v1/client/src/features/register/Register.tsx
@@ -1,14 +1,7 @@
 import axios from "axios";
-import { useState } from "react";
-import { handleMatchPassword } from "./helper";
+import { handleAvailableEmail, handleMatchPassword } from "./helper";
 
 const Register = () => {
-
-    const [registerStep1, setRegisterStep1] = useState(false);
-
-    const handleChange = () => {
-        handleMatchPassword();
-    }
 
     const handleRegister = async (event: React.FormEvent<HTMLFormElement> | any) => {
         try {
@@ -17,17 +10,8 @@ const Register = () => {
                 event.target.elements.email.value,
                 event.target.elements.password.value,
                 event.target.elements.confirmPassword.value
-            ];
+            ];            
 
-            const { data } = await axios.post("/users/register-step-1", { email, password, confirmPassword });
-            if (!data) throw new Error("Couldn't receive data from axios POST : /users/register-step-1");
-            
-            const { error, register } = data;
-            if(error) throw error;
-
-            register && setRegisterStep1(true);
-            
-            
         } catch (error) {
             console.error(error);
         }
@@ -36,9 +20,9 @@ const Register = () => {
 
     return (
         <form onSubmit={handleRegister}>
-            <input type="email" name="email" id="email" placeholder="Enter email" />
+            <input onBlur={handleAvailableEmail} type="email" name="email" id="email" placeholder="Enter email" />
             <input type="password" name="password" id="password" />
-            <input onChange={handleMatchPassword} type="password" name="confirmPassword" id="confirmPassword" />
+            <input onBlur={handleMatchPassword} type="password" name="confirmPassword" id="confirmPassword" />
             <input type="submit" value="Register" id="register" />
         </form>
     )
