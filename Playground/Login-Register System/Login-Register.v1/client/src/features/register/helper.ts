eead7626413b9c9c17ab57dd3685a1c6fa8ae581
lioGR <liorgrishinnew@gmail.com>
2022-10-09 10:15:58 +0300

    Update helper.ts

--- Playground/Login-Register System/Login-Register.v1/client/src/features/register/helper.ts
+++ Playground/Login-Register System/Login-Register.v1/client/src/features/register/helper.ts
@@ -1,10 +1,13 @@
-export function handleMatchPassword () : void {
+import axios from "axios";
+import React from "react";
+
+export function handleMatchPassword(): void {
     try {
         const password = document.getElementById("password") as HTMLInputElement;
         const confirmPassword = document.getElementById("confirmPassword") as HTMLInputElement;
         const registerButton = document.getElementById("register") as HTMLInputElement;
 
-        if(confirmPassword.value !== password.value) {
+        if (confirmPassword.value !== password.value) {
             password.style.borderColor = "red";
             confirmPassword.style.borderColor = "red";
             registerButton.disabled = true;
@@ -18,3 +21,17 @@ export function handleMatchPassword () : void {
     }
 }
 
+export async function handleAvailableEmail(event: React.ChangeEvent<HTMLInputElement>) {
+    try {
+
+        const registerButton = document.getElementById("register") as HTMLInputElement;
+
+        const email = event.target.value;
+        const { data } = await axios.post("/users/available-email", { email });
+        if(!data) throw new Error("Couldn't receive data from AXIOS POST: /users/available-email");
+        const { available } = data;
+        available ? event.target.style.borderColor = "green" : event.target.style.borderColor = "red";
+    } catch (error) {
+        console.error(error);
+    }
+}
