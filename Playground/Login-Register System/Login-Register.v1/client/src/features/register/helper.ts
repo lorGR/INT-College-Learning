837824e4ec653112f6fd02918fa569017d31d472
lioGR <liorgrishinnew@gmail.com>
2022-10-09 10:38:48 +0300

    Update helper.ts

--- Playground/Login-Register System/Login-Register.v1/client/src/features/register/helper.ts
+++ Playground/Login-Register System/Login-Register.v1/client/src/features/register/helper.ts
@@ -1,5 +1,22 @@
 import axios from "axios";
 import React from "react";
+import Joi from "joi";
+import { joiPasswordExtendCore } from "joi-password";
+
+const joiPassword = Joi.extend(joiPasswordExtendCore);
+
+export const PasswordSecurity = Joi.object({
+    password: joiPassword
+        .string()
+        .min(6)
+        .max(16)
+        .minOfLowercase(1)
+        .minOfUppercase(1)
+        .minOfNumeric(1)
+        .minOfSpecialCharacters(1)
+        .noWhiteSpaces()
+        .required(),
+});
 
 export function handleMatchPassword(): void {
     try {
@@ -28,7 +45,7 @@ export async function handleAvailableEmail(event: React.ChangeEvent<HTMLInputEle
 
         const email = event.target.value;
         const { data } = await axios.post("/users/available-email", { email });
-        if(!data) throw new Error("Couldn't receive data from AXIOS POST: /users/available-email");
+        if (!data) throw new Error("Couldn't receive data from AXIOS POST: /users/available-email");
         const { available } = data;
         if (available) {
             event.target.style.borderColor = "green";
@@ -37,6 +54,16 @@ export async function handleAvailableEmail(event: React.ChangeEvent<HTMLInputEle
             event.target.style.borderColor = "red";
             registerButton.disabled = true;
         }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export function handlePasswordSecurity(event: React.ChangeEvent<HTMLInputElement>) {
+    try {
+        const password = event.target.value;
+        const { error } = PasswordSecurity.validate({ password });
+        if (error) throw error;
     } catch (error) {
         console.error(error);
     }
