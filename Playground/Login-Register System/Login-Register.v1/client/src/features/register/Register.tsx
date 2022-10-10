840cfb250c6eb11da75fda68528f35301ca6100e
lioGR <liorgrishinnew@gmail.com>
2022-10-10 12:01:30 +0300

    Update Register.tsx

--- Playground/Login-Register System/Login-Register.v1/client/src/features/register/Register.tsx
+++ Playground/Login-Register System/Login-Register.v1/client/src/features/register/Register.tsx
@@ -1,16 +1,25 @@
 import axios from "axios";
+import { useState } from "react";
+import HandlingErrors from "../handlingErrors/HandlingErrors";
 import { handleAvailableEmail, handleMatchPassword, handlePasswordSecurity } from "./helper";
 
 const Register = () => {
 
+    const [validEmail, setValidEmail] = useState<any>();
+    const [validPassword, setValidPassword] = useState<any>();
+    const [validConfirmPassword, setValidConfirmPassword] = useState<boolean | string>();
+
     const handleRegister = async (event: React.FormEvent<HTMLFormElement> | any) => {
+        event.preventDefault();
         try {
-            event.preventDefault();
             const [email, password, confirmPassword]: Array<string> = [
                 event.target.elements.email.value,
                 event.target.elements.password.value,
                 event.target.elements.confirmPassword.value
-            ];            
+            ];
+            setValidEmail(handleAvailableEmail);
+            setValidPassword(handlePasswordSecurity);
+            setValidConfirmPassword(handleMatchPassword);
 
         } catch (error) {
             console.error(error);
@@ -24,6 +33,7 @@ const Register = () => {
             <input onBlur={handlePasswordSecurity} type="password" name="password" id="password" />
             <input onBlur={handleMatchPassword} type="password" name="confirmPassword" id="confirmPassword" />
             <input type="submit" value="Register" id="register" />
+            <HandlingErrors />
         </form>
     )
 }
