44e3de1ce015042d0ec35c3dfe57facadda23a42
lioGR <liorgrishinnew@gmail.com>
2022-10-10 12:01:48 +0300

    Create server.ts

---
+++ Playground/Login-Register System/Login-Register.v2/server/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT;
+const mongodbURI = process.env.MONGODB_URI;
+
+app.use(express.static("../public/build"));
+app.use(express.json());
+
+if (mongodbURI) {
+    mongoose
+        .connect(mongodbURI)
+        .then(() => { console.log("Conncted to DB"); })
+        .catch(() => { console.log("Couldn't connect to DB"); })
+} else {
+    console.log("Couldn't find mongodbURI");
+}
+
+import UserRoutes from "./features/users/UserRoutes";
+app.use("/users", UserRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running http://localhost:${port}`);
+});
