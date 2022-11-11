d49e15533e226d4f8f569d0055cfbea40667205b
lioGR <liorgrishinnew@gmail.com>
2022-11-11 18:11:19 +0200

    wip

--- SQL/01 - First SQL/server.ts
+++ SQL/01 - First SQL/server.ts
@@ -1,27 +1,21 @@
 import express from "express";
 import mysql from "mysql";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
 const port = 4000;
-const sqlPassword = process.env.PASSWORD;
 
 app.use(express.json());
 
 const connection = mysql.createConnection({
     host: "localhost",
-    port: "3306",
     user: "root",
     password: "password",
-    database: "class_exersice_01"
+    database: "firstDB"
 });
 
 connection.connect((err: any) => {
     try {
         if (err) throw err;
-
         console.info(`Connected to MySQL`);
     } catch (error) {
         console.error(error);
@@ -29,41 +23,83 @@ connection.connect((err: any) => {
 });
 
 app.post("/api/create-table", (req: express.Request, res: express.Response) => {
-    const query = `CREATE TABLE class_exersice_01.teachers (
-        teacher_id INT NOT NULL AUTO_INCREMENT,
-        first_name VARCHAR(45) NOT NULL,
-        last_name VARCHAR(45) NOT NULL,
-        PRIMARY KEY (teacher_id),
-        UNIQUE INDEX teacher_id_UNIQUE (teacher_id ASC) VISIBLE)`;
-
-    connection.query(query, (err, results, fields) => {
-        try {
+    try {
+        const sql = `CREATE TABLE persons (id INT AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(64), last_name VARCHAR(64))`;
+        connection.query(sql, (err, result) => {
             if (err) throw err;
-            console.log(results);
-            console.log(fields);
-            res.send({ created: true, message: `table was created` });
-        } catch (error) {
-            res.send({ error: error.message });
-        }
-
-    });
+            console.log(result);
+            res.send({ msg: "Table created" });
+        })
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
 });
 
-app.post("/api/add-row", (req: express.Request, res: express.Response) => {
-    const { first_name , last_name} = req.body;
-    const query = `INSERT INTO class_exersice_01.teachers (first_name, last_name) VALUES ("${first_name}", "${last_name}")`;
-    
-    connection.query(query, (err, result, fields) => {
-        try {
-            if(err) throw err;
-            console.log(result);
-            console.log(fields);
-            res.send({created: true, message: "added row"});
-        } catch (error) {
-            res.send({error: error.message, created: false});
-        }
-    })
+app.post("/api/alter-person", (req: express.Request, res: express.Response) => {
+    try {
+        const { colName, colType } = req.body;
+        if (!colName || !colType) throw new Error(`Couldn't recieve colName or colType from req.body`);
 
+        const sql = `ALTER TABLE persons ADD COLUMN ${colName} ${colType}`;
+        connection.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.send({ msg: `altered persons table with ${colName} column typed as ${colType}` });
+        });
+
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+
+app.post("/api/insert-person", (req: express.Request, res: express.Response) => {
+    try {
+        const { first_name, last_name } = req.body;
+        if (!first_name || !last_name) throw new Error("first_name and last_name required and wasn't found by req.body");
+
+        const sql = `INSERT INTO persons (first_name,last_name) VALUES ("${first_name}","${last_name}")`;
+        connection.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.send({ msg: `inserted new person first_name: ${first_name} last_name: ${last_name}` });
+        });
+
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+
+app.post("/api/update-year-person", (req: express.Request, res: express.Response) => {
+    try {
+        const { year, id } = req.body;
+        if (!year || !id) throw new Error("Couldn't receive year or id of the person from req.body");
+
+        const sql = `UPDATE firstDB.persons SET birth_year = ${year} WHERE (id = ${id})`;
+        connection.query(sql, (err, results) => {
+            if (err) throw err;
+            console.log(results);
+            res.send({ msg: `updated person with id: ${id} with birth_year to: ${year}` });
+        });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+})
+
+app.post("/api/person-by-year", (req: express.Request, res: express.Response) => {
+    try {
+        const { year } = req.body;
+        if (!year) throw new Error("Couldn't recieve year from req.body");
+
+        const sql = `SELECT * FROM persons WHERE birth_year = ${year}`;
+
+        connection.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.send({ result, msg: `Found the persons with that birth year of them is: ${year}` });
+        });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
 })
 
 app.listen(port, () => {
