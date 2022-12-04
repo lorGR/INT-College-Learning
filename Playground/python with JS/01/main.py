6f4a87715095c961464c367a2eff3c88fdb9959b
lioGR <liorgrishinnew@gmail.com>
2022-12-04 17:55:00 +0200

    WIP

---
+++ Playground/python with JS/01/main.py
@@ -0,0 +1,17 @@
+# def greetings(name):
+    # print(f'Hi, {name}')
+    # return (name)
+
+# greetings('Lior')
+
+# def info():
+    # class Person:
+        # def __init__():
+            
+            
+
+# def add(num1, num2):
+    # return num1 + num2
+
+# def subtract(num1, num2):
+    # return num1 - num2
