a4aac194d6bf43e7b10f31a8236bb135bf458a9c
lioGR <liorgrishinnew@gmail.com>
2022-12-20 19:15:47 +0200

    WIP

---
+++ Tutoring Friends/FS-Oct/03 - Typescript/example/dist/script.js
@@ -0,0 +1,30 @@
+// var salary1 = 1000;
+// var salary2 = 2000;
+// var salary3 = 3000;
+// var salary4 = 4000;
+// var salary5 = 5000;
+// var salary6 = 6000;
+// var salary7 = 7000;
+// var salary8 = 8000;
+// var salary9 = 9000;
+// var salary10 = 10000;
+// var salaryAvg = (salary1 + salary2 + salary3 + salary4 + salary5 + salary6 + salary7 + salary8 + salary9 + salary10) / 10;
+// console.log(`The average salary of developer ${salaryAvg}`);
+// var myNumber = 2;
+// var power = 1;
+// console.log(Math.pow(myNumber, power));
+var myAge = 21;
+var barAgeMinmum = 18;
+var barVipMinmum = 21;
+if (myAge >= barAgeMinmum) {
+    console.log('Welcome to the bar');
+    if (myAge >= barVipMinmum) {
+        console.log('Welcome to the VIP');
+    }
+    else {
+        console.log("We're sorry the minimum age to get the vip room is: " + barVipMinmum);
+    }
+}
+else {
+    console.log("Your under age to come in");
+}
